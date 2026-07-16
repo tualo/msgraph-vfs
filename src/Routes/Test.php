4220857563a16d2f34467d79a5d0185bf846502a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-07-16 13:57:32 +0200

    Version bump to 1.0.9

--- src/Routes/Test.php
+++ src/Routes/Test.php
@@ -10,19 +10,28 @@ class Test extends \Tualo\Office\Basic\RouteWrapper
 {
     public static function register()
     {
-        BasicRoute::add('/msgraph-vfs/(?P<urlEncodedSharepointURL>.+)', function ($matches) {
+        BasicRoute::add('/msgraph-vfs(?:/(?P<folderPath>.*))?$', function ($matches) {
             App::contenttype('application/json');
 
             try {
-                $sharePointUrl = urldecode($matches['urlEncodedSharepointURL']);
+                // $sharePointUrl = urldecode($matches['urlEncodedSharepointURL']);
+                $sharePointUrl = "https://tualo.sharepoint.com/sites/wvd-tualo/";
                 $resolved = VFS::resolveSharePointIds($sharePointUrl);
                 App::result('resolved', $resolved);
 
                 VFS::registerVFS($sharePointUrl);
 
                 $scheme = App::configuration('msgraph-vfs', 'scheme', 'msgraph-vfs');
+                $folderPath = isset($matches['folderPath']) ? trim(urldecode($matches['folderPath']), '/') : '';
+                $directoryUrl = $scheme . '://' . ($folderPath !== '' ? $folderPath : '');
+
+                if ($folderPath !== '' && !is_dir($directoryUrl)) {
+                    mkdir($directoryUrl, 0777, true);
+                }
+
+                file_put_contents($directoryUrl . '/test.txt', 'Inhalt');
                 $entries = [];
-                $handle = opendir($scheme . '://');
+                $handle = opendir($directoryUrl);
                 if ($handle !== false) {
                     while (($entry = readdir($handle)) !== false) {
                         if ($entry !== '.' && $entry !== '..') {
